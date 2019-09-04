54346cca32fb57e400cdf258c2cc572fefb8f717
t <t@192.168.0.102>
2019-09-04 22:05:40 +0800

    qweq

---
+++ ok/ok-future/src/main/java/com/nixi/OkFutureApplication.java
@@ -0,0 +1,13 @@
+package com.nixi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkFutureApplication.class, args);
+	}
+
+}
